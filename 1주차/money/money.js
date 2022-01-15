1b7795d4b810dc8e0f4c64828aae1c7a1c3ea17b
rachel490 <ccori12@naver.com>
2022-01-15 17:12:13 +0900

    [ch1] add multiplication function

---
+++ 1주차/money/money.js
@@ -0,0 +1,11 @@
+class Dollar {
+    constructor(amount) {
+        this.amount = amount;
+    }
+
+    times(multiplier) {
+        return this.amount *= multiplier
+    }
+}
+
+module.exports = Dollar;
